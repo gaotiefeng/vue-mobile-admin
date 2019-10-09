d7b309d456c7853408a0d5d8e64c8860e59e97b2
高铁峰 <1096392101@qq.com>
2019-10-09 10:44:17 +0800

    update

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="admin">
-        <el-table
+      <el-table
       :data="list"
       style="width: 100%">
-      <el-table-column
+        <el-table-column
         prop="username"
         label="名称"
         width="180">
@@ -48,70 +48,72 @@
 import { getList,del } from '@/api/admin'
 import * as error from '@/utils/error'
 export default {
-    data() {
-        return {
-          list: [],
-          total: 1,
-          page: 1,
-          search: {
-            'offset': 0,
-            'limit': 10,
-          },
-        }
+data() {
+    return {
+      list: [],
+      total: 1,
+      page: 1,
+      search: {
+        'offset': 0,
+        'limit': 10,
       },
-    created() {
-        this.admin();
+    }
+  },
+created() {
+    this.admin();
+},
+methods: {
+    async admin(){
+        const query = this.search;
+        let res = await getList(query);
+        if(res.code == error.SUCCESS_CODE) {
+            this.list = res.data.items;
+            this.total = res.data.count;
+        }
     },
-    methods: {
-        async admin(){
-            const query = this.search;
-            let res = await getList(query);
-            if(res.code == error.SUCCESS_CODE) {
-                this.list = res.data.items;
-                this.total = res.data.count;
-            }
-        },
-        // 分页
-        handleCurrentChange(currentPage) {
-        Object.assign(this.search, {
-            offset: (currentPage - 1) * this.search.limit
-        });
-        this.admin();
-        },
-        async del(id) {
+    // 分页
+    handleCurrentChange(currentPage) {
+    Object.assign(this.search, {
+        offset: (currentPage - 1) * this.search.limit
+    });
+    this.admin();
+    },
+    async del(id) {
 
-          let query = {
-            'id' : id
-          }
-          this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-          }).then(() => {
-           del(query).then(result => {
-           if(result.code == error.SUCCESS_CODE)
-            {
-              this.$message({
-              type: 'success',
-              message: '删除成功!'
-              });
-            }
-            this.admin();
-           })
-          
-          }).catch(() => {
+      let query = {
+        'id' : id
+      }
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+      }).then(() => {
+        del(query).then(result => {
+        if(result.code == error.SUCCESS_CODE)
+        {
           this.$message({
-            type: 'info',
-            message: '已取消删除'
-          });          
-        });
-        },
+          type: 'success',
+          message: '删除成功!'
+          });
+        }
+        this.admin();
+        })
+      
+      }).catch(() => {
+      this.$message({
+        type: 'info',
+        message: '已取消删除'
+      });          
+    });
     },
 }
-</script>
+}
+</script> 
+
 <style scoped>
 .admin {
   margin-left: 40px;
   text-align: center;
 }
+
 </style>
